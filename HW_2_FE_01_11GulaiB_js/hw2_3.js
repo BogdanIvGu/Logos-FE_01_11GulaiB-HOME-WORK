ef3bd1c1a651db5a4cc2de38ef499628369fb249
Bogdan <bogdan.i.gulai@lpnu.ua>
2023-01-18 22:31:50 +0200

    3 Задача 2 домашки

---
+++ HW_2_FE_01_11GulaiB_js/hw2_3.js
@@ -0,0 +1,47 @@
+// Перша задача
+
+let numberOfManth = parseInt(prompt(" Введіть номер місяця" , "Значення від 1 до 12"));
+
+if (numberOfManth == 1) {
+    alert ("Січень");
+}
+else if (numberOfManth == 2) {
+    alert("Лютий");
+
+}
+else if (numberOfManth == 3) {
+    alert("Березень");
+}
+else if (numberOfManth == 4) {
+    alert("Квітень");
+}
+else if (numberOfManth == 5) {
+    alert("Травень");
+}
+else if (numberOfManth == 6) {
+    alert("Червень");
+}
+else if (numberOfManth == 7) {
+    alert("Липень");
+}
+else if (numberOfManth == 8) {
+    alert("Серпень");
+}
+else if (numberOfManth == 9) {
+    alert("Вересень");
+}
+else if (numberOfManth == 10) {
+    alert("Жовтень");
+}
+else if (numberOfManth == 11) {
+    alert("Листопад");
+}
+else if (numberOfManth == 12) {
+    alert("Грудень");
+}
+else if (numberOfManth == null) {
+    alert("Чому ви нічого не ввели ? Спробуйте ввести номер місяця згідно рекомендацій");
+}
+else  {
+    alert("Невже так важко виконати рекомендації і ввести число від 1 до 12 ??? Ану спробуйТе ще раз ввести, але цього разу без жартів!!!");
+}
