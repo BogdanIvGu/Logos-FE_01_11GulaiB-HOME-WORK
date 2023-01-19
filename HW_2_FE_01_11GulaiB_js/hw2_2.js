bfb844274875ad8e34314257e02cd7d2f66743f3
Bogdan <bogdan.i.gulai@lpnu.ua>
2023-01-19 17:49:14 +0200

    19012023

---
+++ HW_2_FE_01_11GulaiB_js/hw2_2.js
@@ -0,0 +1,28 @@
+// Друга задача
+
+// Працює не коректно. Допоможіть розібратись. не працює на скасування
+
+let userName = "name";
+let userPasword = "Логос";
+
+let nameIn = prompt("Введіть імя Латиницею", " Use name");
+if (nameIn == userName) {
+    let paswordIn = prompt(" Введіть пароль Кирилицею", " Use your pasword");
+     if (paswordIn == userPasword) {
+        alert("Ласкаво просимо!");
+    }
+    else if (paswordIn != userPasword ) {
+        alert("Пароль невірний!");
+    }
+    else {
+        alert("Вхід скасований");
+    }
+}
+
+else if (nameIn != userName) {
+    alert("Я Вас не знаю!");
+}
+else {
+    alert("Вхід скасований");
+}
+
