a1418e06387ec4d67227fc0eb7c44b667c1d750f
Bogdan <bogdan.i.gulai@lpnu.ua>
2023-01-23 18:41:59 +0200

    Оновлено

--- HW_2_FE_01_11GulaiB_js/hw2_2.js
+++ HW_2_FE_01_11GulaiB_js/hw2_2.js
@@ -2,29 +2,53 @@
 
 // Працює не коректно. Допоможіть розібратись. не працює на скасування
 
-let userName = "name";
-let userPasword = "Логос";
+// let userName = "name";
+// let userPasword = "Логос";
 
-let nameIn = prompt("Введіть імя Латиницею", " Use name");
-if (nameIn == userName) {
-    let paswordIn = prompt(" Введіть пароль Кирилицею", " Use your pasword");
-     if (paswordIn == userPasword) {
-        alert("Ласкаво просимо!");
-    }
-    else if (paswordIn != userPasword ) {
-        alert("Пароль невірний!");
-    }
-    // на скасування не працюєю
-    else {
-        alert("Вхід скасований");
-    }
-}
+// let nameIn = prompt("Введіть імя Латиницею", " Use name");
+// if (nameIn == userName) {
+//     let paswordIn = prompt(" Введіть пароль Кирилицею", " Use your pasword");
+//      if (paswordIn == userPasword) {
+//         alert("Ласкаво просимо!");
+//     }
+//     else if (paswordIn != userPasword ) {
+//         alert("Пароль невірний!");
+//     }
+//     // на скасування не працюєю
+//     else {
+//         alert("Вхід скасований");
+//     }
+// }
 
-else if (nameIn != userName) {
-    alert("Я Вас не знаю!");
-}
-// на скасування не працюєю
-else {
+// else if (nameIn != userName) {
+//     alert("Я Вас не знаю!");
+// }
+// // на скасування не працюєю
+// else {
+//     alert("Вхід скасований");
+// }
+
+const userName = "Name";
+const paswordIn = "Логос";
+let namedIn = prompt(
+  " Введіть імя Латиницею",
+  "Use your name",
+);
+
+if (namedIn === null) {
+  alert("Вхід скасований");
+} else if (namedIn !== userName) {
+  alert("Я вас не знаю!");
+} else if (namedIn === userName) {
+  let passIn = prompt(
+    " Введіть пароль Кирилицею",
+    " Use your pasword",
+  );
+  if (passIn === null) {
     alert("Вхід скасований");
+  } else if (passIn !== paswordIn) {
+    alert("Пароль невірний!");
+  } else if (passIn === paswordIn) {
+    alert("Ласкаво просимо!");
+  }
 }
-
