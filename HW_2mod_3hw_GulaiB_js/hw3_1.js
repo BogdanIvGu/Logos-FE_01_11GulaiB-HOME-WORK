6a0f112cdc7101be13826bfd170104a658ee7c16
Bogdan <bogdan.i.gulai@lpnu.ua>
2023-02-10 20:47:15 +0200

    Обновлено

--- HW_2mod_3hw_GulaiB_js/hw3_1.js
+++ HW_2mod_3hw_GulaiB_js/hw3_1.js
@@ -44,8 +44,10 @@
 
                                 // Завдання 3
 
-// for (let c = 1; c <=55; c += 2) {
-//     document.write (` <h1> Число  ${c} </h1> `);
+// let resalt = -1;
+// for (let c = 1;  c <=55; c ++) {
+//     resalt += 2;
+//     document.write (` <h1> Число  ${resalt} </h1> `);
 //     console.log(c);
 //     }
 
@@ -65,7 +67,7 @@
 //     console.log(result);
 //     }
 
-// інший варіант
+// інший варіант де більше дій в дужках циклу for/  Воно працює. Якщо так робити не можна, то поясніть чому.
 
 // for (let a = 1, result = 2; a <=20; a++, result *= 2) {
 //     document.write (` <h1> Число  ${result} </h1> `);
@@ -92,25 +94,44 @@
 
 
                                 // Завдання 8
-let a = prompt('Введіть число для обчислення степеня' , 'Число');
-let b = prompt('Введіть значення степеня');
-let resalt =1;
+let a = +prompt('Введіть число для обчислення степеня' , 'Число');
+let b = +prompt('Введіть значення степеня');
+let result =1;
 
 for (let i = 1; i <= Math.abs(b); i++) { 
     if (b > 0){
-        resalt*=a;
+        result*=a;
         console.log(a);
     }
     else if (b < 0) {
-        resalt = (1/(resalt*a));
-        console.log(a);
+        result /=a;
+        console.log(result);
     }
-    else if (b == 0) {resalt = 1;
-        console.log(resalt);
+    else if (b == 0) {result = 1;
+        console.log(result);
     }
     else {
         document.write ('Значення введені не вірно');
     }
-    console.log(resalt);
+    console.log(result);
 }
-document.write (` <h1> Значення приведення степеня  ${resalt} </h1> `);
+document.write (` <h1> Значення приведення степеня  ${result} </h1> `);
+
+ 
+
+                                // інший варіант 8
+
+// let number = prompt("Input number");
+// let power = prompt("Input pow");
+// let result = Math.pow(number, power);
+// console.log(result);
+// document.write (` <h1> Значення приведення степеня  ${result} </h1> `);
+
+
+
+// №3 та №8 неправильно виводить результат
+
+// також цикл for має в дужках три дії ітерації  -  НЕ ФАКТ.
+
+// виправіть всюди де у вас є хзайві дії в дужках   - Можна в дужках вказувати і більше дій. Я це бачив на інших навчальних курсах в Ютубі. 
+// Я це зробив для досвіду, для експерименту, перевірити чи так працює. Працює. Якщо так не можна то поясніть чому ))
