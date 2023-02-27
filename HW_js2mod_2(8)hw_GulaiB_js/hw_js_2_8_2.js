10ef12eb8a349bf1e912b4eabe29cc3e7ab1ab13
Bogdan <bogdan.i.gulai@lpnu.ua>
2023-02-27 17:55:24 +0200

    27022023

---
+++ HW_js2mod_2(8)hw_GulaiB_js/hw_js_2_8_2.js
@@ -0,0 +1,64 @@
+                            // // Перша задача
+
+// let text = prompt("Введіть текст:");
+// let spaces = text.split(" ").length - 1;
+// console.log(`Кількість пробілів: ${spaces}`);
+// alert(`Кількість пробілів: ${spaces}`);
+
+                  // Друга  задача
+
+
+// let email = prompt("Введіть email-адресу:");
+// if (email.indexOf("@") > 0 && email.indexOf("@") < email.length - 1) {
+//   console.log("Email-адреса введена правильно");
+// } else {
+//   console.log("Email-адреса введена некоректно");
+// }
+
+
+// Третя задача
+
+
+// let input = prompt("Введіть текст:");
+// let words = input.split(" "); 
+// console.log(words); // перевіряю що вивело ))
+// let count = 0;
+// for (let i = 0; i < words.length; i++) {
+//   if (words[i].toLowerCase() === "html") { 
+//     count++;
+//   }
+// }
+// console.log(count);
+
+
+// Четверта задача
+// // Варіант1
+// const url = prompt("Введіть URL адресу ");
+// if(url.startsWith("https://")) {
+// console.log (url.slice(8).toLowerCase());
+// }
+// else if (url.startsWith("http://")){
+//   console.log (url.slice(7).toLowerCase());
+//   }
+// else {
+//   console.log (url.toLowerCase());
+// }
+
+// // Варіант2
+// const url = prompt("Введіть URL адресу ");
+// const index = url.indexOf("www");
+// const output = url.slice(index);
+// console.log(output.toLowerCase());
+
+
+
+
+
+// варіант чату GPT
+// const url = prompt("Введіть URL адресу ");
+// const dom = url.replace(/^(https?:\/\/)?(www\.)?/i, '').split('/')[0];
+
+// console.log(dom);
+
+
+
