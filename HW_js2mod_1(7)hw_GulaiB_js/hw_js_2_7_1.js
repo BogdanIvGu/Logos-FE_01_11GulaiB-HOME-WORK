5dc0d7203a64781c4cfd77b648613fb5a876c4fb
Bogdan <bogdan.i.gulai@lpnu.ua>
2023-02-27 18:57:39 +0200

    27022023

--- HW_js2mod_1(7)hw_GulaiB_js/hw_js_2_7_1.js
+++ HW_js2mod_1(7)hw_GulaiB_js/hw_js_2_7_1.js
@@ -1,42 +1,35 @@
-// // Перша задача
+// // // Перша задача
 
-let person = {};
-person.firstName = "Ivan";
-person.secondName = "Ivanov";
-person.showData = function () {
-  console.log(this.firstName + " " + this.secondName);
-};
-let newPerson = Object.assign({}, person);
-newPerson.firstName = "Petro";
-newPerson.secondName = "Petriv";
-person.showData(); // поверне Ivan Ivanov
-newPerson.showData(); // поверне Petro Petriv
+// let person = {};
+// person.firstName = "Ivan";
+// person.secondName = "Ivanov";
+// person.showData = function () {
+//   console.log(this.firstName + " " + this.secondName);
+// };
+// let newPerson = Object.assign({}, person);
+// newPerson.firstName = "Petro";
+// newPerson.secondName = "Petriv";
+// console.log(person.showData());
+// console.log(newPerson.showData());
 
-// Друга  задача
+// // Друга  задача
 
 let MyMath = {};
-
 MyMath.a = 5;
-
 MyMath.b = 2;
-
 MyMath.sum = function () {
   console.log(this.a + this.b);
 };
-
 MyMath.multiplication = function () {
   console.log(this.a * this.b);
 };
-
 MyMath.division = function () {
   console.log(this.a / this.b);
 };
-
 MyMath.subtraction = function () {
   console.log(this.a - this.b);
 };
-
-MyMath.sum();
-MyMath.multiplication();
-MyMath.division();
-MyMath.subtraction();
+console.log(MyMath.sum());
+console.log(MyMath.multiplication());
+console.log(MyMath.division());
+console.log(MyMath.subtraction());
