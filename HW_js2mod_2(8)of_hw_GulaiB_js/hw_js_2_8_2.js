b35b8f6ed84234fda373342252a7464e9d588609
Bogdan <bogdan.i.gulai@lpnu.ua>
2023-03-02 13:56:57 +0200

    02032023

--- HW_js2mod_2(8)of_hw_GulaiB_js/hw_js_2_8_2.js
+++ HW_js2mod_2(8)of_hw_GulaiB_js/hw_js_2_8_2.js
@@ -1,3 +1,37 @@
                             // // Перша задача
 
-const Iperson = {};
+/ // // Перша задача
+
+// let person = {};
+// person.firstName = "Ivan";
+// person.secondName = "Ivanov";
+// person.showData = function () {
+//   console.log(this.firstName + " " + this.secondName);
+// };
+// let newPerson = Object.assign({}, person);
+// newPerson.firstName = "Petro";
+// newPerson.secondName = "Petriv";
+// console.log(person.showData());
+// console.log(newPerson.showData());
+
+// // Друга  задача
+
+let MyMath = {};
+MyMath.a = 5;
+MyMath.b = 2;
+MyMath.sum = function () {
+  console.log(this.a + this.b);
+};
+MyMath.multiplication = function () {
+  console.log(this.a * this.b);
+};
+MyMath.division = function () {
+  console.log(this.a / this.b);
+};
+MyMath.subtraction = function () {
+  console.log(this.a - this.b);
+};
+console.log(MyMath.sum());
+console.log(MyMath.multiplication());
+console.log(MyMath.division());
+console.log(MyMath.subtraction());
