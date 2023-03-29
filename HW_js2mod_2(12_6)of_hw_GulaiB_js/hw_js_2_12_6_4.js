80637c2a75902d9d85a53666c448b68550c995fb
BogdanIvGu <121254043+BogdanIvGu@users.noreply.github.com>
2023-03-29 19:22:21 +0300

    29032023

--- HW_js2mod_2(12_6)of_hw_GulaiB_js/hw_js_2_12_6_4.js
+++ HW_js2mod_2(12_6)of_hw_GulaiB_js/hw_js_2_12_6_4.js
@@ -8,12 +8,22 @@
 // });
 
 
+// let colorList = document.getElementById('color-list');
+
+// colorList.addEventListener('click', function(event) {
+//     if (event.target.tagName === 'LI') {
+//         let color = event.target.textContent;
+//         event.target.style.color = color;
+//     }
+// });
+
 let colorList = document.getElementById('color-list');
+let previousColor = 'black'; // зберігаємо попередній колір
 
 colorList.addEventListener('click', function(event) {
     if (event.target.tagName === 'LI') {
         let color = event.target.textContent;
-        event.target.style.color = color;
+        event.target.style.color = color === previousColor ? 'black' : color; // перевіряємо, чи колір співпадає з попереднім
+        previousColor = event.target.style.color; // зберігаємо поточний колір для наступного натискання
     }
 });
-
